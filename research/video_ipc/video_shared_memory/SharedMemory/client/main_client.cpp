0c1c79d12e59a373524fc42bf8601a4028848b21
Rafal Kruszyna <rafal.kruszyna@gmail.com>
2024-05-25 16:45:02 +0200

    refactor

--- research/video_ipc/video_shared_memory/SharedMemory/client/main_client.cpp
+++ research/video_ipc/video_shared_memory/SharedMemory/client/main_client.cpp
@@ -1,14 +1,15 @@
 #include <QApplication>
-#include "TriangleWidget.h"
+#include "ClientWidget.hpp"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    TriangleWidget w;
+    ClientWidget w;
     w.resize(1700, 900);
     w.show();
 
     return a.exec();
 }
+
 
